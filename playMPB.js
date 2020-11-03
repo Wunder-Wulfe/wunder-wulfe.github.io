97f9d555388e3d668021c2dbbb49d16cf96a1adb
Wunder Wulfe <doge_is_swagaroony@hotmail.com>
2020-11-03 16:46:15 -0600

    Update playMPB.js

--- playMPB.js
+++ playMPB.js
@@ -4,16 +4,16 @@ const reasonBox = $("#reason");
 
 const Reponses = [
 	["warn", "Maybe?", "Theres a few sweats online"],
-  ["warn", "Maybe?", "The server is pretty empty"],
-  ["warn", "Maybe?", "The ping is prety high"],
-  ["alert", "No.", "Nobody is playing"],
-  ["success", "Sure.", "There's quite a few people playing"]
+	["warn", "Maybe?", "The server is pretty empty"],
+	["warn", "Maybe?", "The ping is prety high"],
+	["alert", "No.", "Nobody is playing"],
+	["success", "Sure.", "There's quite a few people playing"]
 ];
 
 const sweats = [
 	347826216,
-  284533122,
-  194829277
+	284533122,
+	194829277
 ];
 
 const get = function(url) {
@@ -34,29 +34,29 @@ const severity = val => {
 
 const pickResponse = function(index) {
 	if (severity(resultBox.class))
-    [
-      resultBox.class, 
-      resultBox.textContent, 
-      reasonBox.textContent
-    ] = Responses[index];
+		[
+			resultBox.class, 
+			resultBox.textContent, 
+			reasonBox.textContent
+		] = Responses[index];
 }
 
 const answer = async function() {
 	const result = await get(endpoint);
-  let maxPlaying = 0;
-  let sweats = 0;
-  pickResponse(4);
- 	for (let server of result.data) {
-  	maxPlaying = Math.max(maxPlaying, server.playing);
-    if (contains(server.playerIds, sweats))
-    	pickResponse(0);
-    else if (server.ping >= 100)
-    	pickResponse(2);
-  }
-  if (result.data.length == 0)
-  	pickResponse(3);
-  else if (maxPlaying <= 5)
-  	pickResponse(1);
+	let maxPlaying = 0;
+	let sweats = 0;
+	pickResponse(4);
+	for (let server of result.data) {
+		maxPlaying = Math.max(maxPlaying, server.playing);
+		if (contains(server.playerIds, sweats))
+			pickResponse(0);
+		else if (server.ping >= 100)
+			pickResponse(2);
+	}
+	if (result.data.length == 0)
+		pickResponse(3);
+	else if (maxPlaying <= 5)
+		pickResponse(1);
 }
 
 answer();
