359ab572dc87ead2a26498d79d599e2bf0ee6d1b
Wunder Wulfe <doge_is_swagaroony@hotmail.com>
2020-11-03 17:26:37 -0600

    Update playMPB.php

--- playMPB.php
+++ playMPB.php
@@ -1,1 +1,3 @@
-file_get_contents("//https://games.roblox.com/v1/games/4953508391/servers/Public");
+<?php
+  echo file_get_contents("//https://games.roblox.com/v1/games/4953508391/servers/Public");
+?>
